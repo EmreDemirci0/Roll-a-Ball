242c3ec545c4f1844fdef4637518bd803d94d831
Emre Demirci <yemre.1268@gmail.com>
2023-09-05 02:58:55 +0300

    +Game Loaded

---
+++ Assets/Scripts/TopKontrol.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TopKontrol : MonoBehaviour
+{
+    Rigidbody fizik;
+    Vector3 baslangic;
+    int sayac = 0;
+    public int ToplanilacakObjeSayisi;
+    public int hiz=5;
+    public Text sayacText;
+    public Text oyunbittiText;
+    private float yatay;
+    private float dikey;
+    void Start()
+    {
+        fizik = GetComponent<Rigidbody>();
+        baslangic = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+         yatay = Input.GetAxisRaw("Horizontal");
+         dikey = Input.GetAxisRaw("Vertical");
+
+        Vector3 vec = new Vector3(yatay,0,dikey);
+
+        fizik.AddForce(vec*hiz);
+        if (sayac == ToplanilacakObjeSayisi)
+        {//OYUN BITTI
+            oyunbittiText.text = "OYUN BITTI";
+            transform.position = baslangic;
+
+        }
+
+    }
+    private void OnTriggerEnter(Collider col)
+    {   
+        if(col.gameObject.tag=="engel")
+        {
+            //Destroy(col.gameObject);
+            col.gameObject.SetActive(false);
+            sayac++;
+            sayacText.text = "Sayac=" + sayac;
+            if(sayac==ToplanilacakObjeSayisi)
+            {//OYUN BITTI
+                oyunbittiText.text = "OYUN BITTI";
+                transform.position = baslangic;
+                
+            }
+        }
+        
+    }
+}
